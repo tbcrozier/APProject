599db75e4d4fa31465afd2a3a9f4edc2bfbaaf3c
tbcrozier <crozitb0@gmail.com>
2017-04-06 22:06:33 -0500

    Deleted aboutActivity class with dialog containing app information Added AboutActivity class Added quitAcitvity method added listeners to MainMenuActivity and buttons to activity_main_menu

---
+++ app/src/main/java/com/davidroach/approject/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.davidroach.approject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by TBCJr on 4/6/17.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_activity);
+    }
+}
--- app/src/main/java/com/davidroach/approject/MainMenuActivity.java
+++ app/src/main/java/com/davidroach/approject/MainMenuActivity.java
@@ -6,9 +6,11 @@ import android.location.Geocoder;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Html;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 
 import java.io.IOException;
 import java.util.List;
@@ -21,54 +23,33 @@ public class MainMenuActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
 
-        //SetUp for Simple Buttons
-        findViewById(R.id.about_button).setOnClickListener(new AboutActivity());
-        findViewById(R.id.quit_button).setOnClickListener(new QuitActivity());
 
     GeoThread testThread = new GeoThread();
         testThread.addressFromCoords(this, 36.533206,-87.353863); //should return 601 college street
-    }
 
 
 
-    //Class do display Dialog About Page from Main Menu
-    class AboutActivity implements View.OnClickListener {
+            findViewById(R.id.about_button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    //go to about
+                    Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                    startActivity(intent);
 
-        @Override
-        public void onClick(View v) {
+                }
+            });
 
-            String message = "<html>\n" +
-                    "<h1>Play App!</h1>\n" +
-                    "<h2>About</h2>\n" +
-                    "<p>Created by:</br></p>\n" +
-                    "<p>Blake Crozier</br></p>\n" +
-                    "<p>David Roach</br></p>\n" +
-                    "<p>Thanks for visiting our app</p>\n" +
-                    "</html>";
+        findViewById(R.id.quit_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Quit App
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
 
-            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
-            builder.setMessage(Html.fromHtml(message));
-            builder.setPositiveButton("Close", null);
-
-            AlertDialog dialog = builder.create();
-            dialog.show();
-
-        }
-
-    }
-
-    //method for quitting app
-    class QuitActivity implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-            //Quit App
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.addCategory(Intent.CATEGORY_HOME);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(intent);
-
-        }
+            }
+        });
     }
 }
 
