905a45248945953bfe27f69b890fa4bce094d2f1
tbcrozier <crozitb0@gmail.com>
2017-04-12 18:39:21 -0500

    Added: - MainMenuActivity.java -- button listeners - CreateEvent.java - Schedule.java - About.java - about_activity.xml -schedule_activity.xml - create_event_activity.xml -Added activities to manifest

--- app/src/main/java/com/davidroach/approject/MainMenuActivity.java
+++ app/src/main/java/com/davidroach/approject/MainMenuActivity.java
@@ -6,7 +6,7 @@ import android.location.Geocoder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
+import android.view.View;
 import java.io.IOException;
 import java.util.List;
 import java.util.Locale;
@@ -21,9 +21,47 @@ public class MainMenuActivity extends AppCompatActivity {
 
     GeoThread testThread = new GeoThread();
         testThread.addressFromCoords(this, 36.533206,-87.353863); //should return 601 college street
+
+
+        findViewById(R.id.create_event_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(), CreateEvent.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.view_schedule_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to about
+                Intent intent = new Intent(getApplicationContext(), Schedule.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.about_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to about
+                Intent intent = new Intent(getApplicationContext(), About.class);
+                startActivity(intent);
+            }
+        });
+
+        findViewById(R.id.quit_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Quit App
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+
     }
-
-
 
 }
 
