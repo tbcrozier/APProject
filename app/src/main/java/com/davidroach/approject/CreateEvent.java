493a7dcff53decd0af4ab18296e3ca6bbfed1700
tbcrozier <crozitb0@gmail.com>
2017-04-12 22:34:47 -0500

    Added button to CreateEvent.java to return to main menu

--- app/src/main/java/com/davidroach/approject/CreateEvent.java
+++ app/src/main/java/com/davidroach/approject/CreateEvent.java
@@ -1,6 +1,8 @@
 package com.davidroach.approject;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by TBCJr on 4/6/17.
@@ -12,6 +14,18 @@ public class CreateEvent extends About {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.create_event_activity);
+
+
+
+        findViewById(R.id.main_menu_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to about
+                Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
 
 
 
