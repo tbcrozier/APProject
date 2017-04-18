3888d5ee9d7a77b0133b5178f5d13ec3356cd985
droach-dev <mrroachhimself@gmail.com>
2017-04-17 19:16:13 -0500

    Added Rest client skeleton as a placeholder.  Working on this now.

---
+++ app/src/main/java/com/davidroach/approject/RestClient.java
@@ -0,0 +1,56 @@
+package com.davidroach.approject;
+
+/**
+ * Created by droach-dev on 4/17/17.
+ */
+
+public class RestClient {
+
+    //constructor
+    public RestClient(){
+
+    }
+
+    protected void getUserInfo(String userNameIn){
+
+    }
+
+    protected void createNewUser(String userNameIn, String passwordIn, String firstNameIn, String lastNameIn, String emailIn){
+
+    }
+
+    protected void getAllEvents(){
+
+    }
+
+    protected void createNewEvent(String sportNameIn, String locationIn, String dateIn, String timeIn, String eventNameIn, String userNameIn){
+
+    }
+
+
+    protected void getUserRSVP(String userNameIn){
+
+    }
+
+    protected void createNewRSVP(String userNameIn, String eventNameIn){
+
+    }
+
+    protected void getEventRSVP(String eventNameIn){
+
+    }
+
+    protected void getAvailableSports(){
+
+    }
+
+    protected void getFutureEventsBySport(String sportNameIn){
+
+    }
+
+    protected void getFutureEventsByLocation(String locationNameIn){
+
+    }
+
+
+}
