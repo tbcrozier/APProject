428205d8f20e18c4e8149c5cbcedd4610cb3cbb6
droach-dev <mrroachhimself@gmail.com>
2017-04-22 10:24:20 -0500

    Had to change some build gradle stuff to get android studio working after upgrade.

---
+++ app/src/main/java/com/davidroach/approject/MapActivity.java
@@ -0,0 +1,10 @@
+package com.davidroach.approject;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by droach-dev on 4/21/17.
+ */
+
+public class MapActivity extends AppCompatActivity {
+}
