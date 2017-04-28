7e259dc3caa941c8804009cb3317e745f4eccc94
tbcrozier <crozitb0@gmail.com>
2017-04-28 09:45:27 -0500

    Date Picker date format now represents current year

--- app/src/main/java/com/davidroach/approject/About.java
+++ app/src/main/java/com/davidroach/approject/About.java
@@ -13,5 +13,8 @@ public class About extends AppCompatActivity {
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.about_activity);
+
+
+
         }
 }
