a8b1321b2cb9b5b7f8b0268556cca9bbe04b59ec
tbcrozier <crozitb0@gmail.com>
2017-05-01 11:56:56 -0500

    Design changes. Plus added main menu button to Create activity so user doesnt have to back click

--- app/src/main/java/com/davidroach/approject/CreateEvent.java
+++ app/src/main/java/com/davidroach/approject/CreateEvent.java
@@ -151,89 +151,7 @@ public class CreateEvent extends AppCompatActivity {
             }
         });
 
-    //Time Button listener
-   /*     findViewById(R.id.btn_time).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Time Picker Code
-                // Get Current time
-                Calendar c1 = null;
-                final TextView tv_time = (TextView) findViewById(R.id.display_time_TV);
 
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                    c1 = Calendar.getInstance();
-                }
-                int hour = 0;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                    hour = c1.get(Calendar.HOUR_OF_DAY);
-                }
-                int minute = 0;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                    minute = c1.get(Calendar.MINUTE);
-                }
-
-                TimePickerDialog timePickerDialog = new TimePickerDialog(CreateEvent.this,
-                        new TimePickerDialog.OnTimeSetListener() {
-
-                            @Override
-                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
-                                Log.i("----------- :" , "------------");
-                                Log.i("THE HOUR IS :" , "HOUR:" + hourOfDay);
-                                Log.i("----------- :" , "------------");
-                                Log.i("THE MINUTE IS :" , "MIN:" + minute);
-
-                                timestring = hourOfDay + ":" + minute + ":"+"00";
-
-                                tv_time.setText(timestring);
-                            }
-                        }, hour, minute, false);
-                timePickerDialog.show();
-            }
-        });*/
-
-    //Date Button Listener
-        /*findViewById(R.id.btn_date).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-                final TextView tv_date = (TextView) findViewById(R.id.display_date_TV);
-                Calendar c2 = null;
-
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                    c2 = Calendar.getInstance();
-                }
-                int year = 2017;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                    year = c2.get(Calendar.YEAR);
-                    Log.i("THE YEAR IS :" , "Year:" + year);
-                }
-                int month = 0;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                    month = c2.get(Calendar.MONTH);
-                }
-                int day = 0;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-                    day = c2.get(Calendar.DAY_OF_MONTH);
-                }
-
-                DatePickerDialog datePickerDialog = new DatePickerDialog(CreateEvent.this,
-                        new DatePickerDialog.OnDateSetListener() {
-
-                            @Override
-                            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
-
-
-                                datestring = year + "-" + (month+1) + "-" + dayOfMonth ;
-
-                                tv_date.setText(datestring);
-                            }
-                        },year, month, day);
-
-                datePickerDialog.show();
-            }
-        });*/
 
         //Date EditText & Listener
         // initiate the date picker and a button
@@ -265,6 +183,15 @@ public class CreateEvent extends AppCompatActivity {
             }
         });
 
+        //Return to Main Menu button listener
+        findViewById(R.id.main_menu_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //go to main menu
+                Intent intent = new Intent(getApplicationContext(), MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
